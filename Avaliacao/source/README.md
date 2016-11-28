8eef6accdc2fbbf2cf752dc82576c589cb6cdaab
rodrigor <rodrigor@ALPF-NL-L1CCPRX.visanet.corp>
2016-11-28 16:49:27 -0200

    Update

---
+++ Avaliacao/source/README.md
@@ -0,0 +1,3 @@
+# REFERENCES
+
+https://www.javacodegeeks.com/2016/07/writing-consuming-soap-webservice-spring.html
---
+++ Avaliacao/source/src/main/java/br/com/fiap/EsbSoapApplication.java
@@ -0,0 +1,12 @@
+package br.com.fiap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsbSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsbSoapApplication.class, args);
+	}
+}
